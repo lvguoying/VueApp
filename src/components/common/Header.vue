206ebad77cefcfc73fe17c65a90e907073e8adf6
lvguoying <876512359@qq.com>
2018-04-16 12:29:36 +0800

    vue全家桶

---
+++ src/components/common/Header.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header"></div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped>
+.header{
+  height: 1rem;
+  background: #000;
+}
+</style>
---
+++ src/components/movie/Movie.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+        <Common-header></Common-header> 
+            <h1>Movie</h1>
+        <Common-footer></Common-footer>
+    </div>
+</template>
+
+<script>
+import CommonHeader from "@/components/common/Header";
+import CommonFooter from "@/components/common/Footer";
+export default {
+  components: {
+    CommonHeader,
+    CommonFooter
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
